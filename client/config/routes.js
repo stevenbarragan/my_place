40d20f08b0d93593ea2dff649fa88071702ad69c
Steven Barragan <me@steven.mx>
2014-05-22 10:30:13 -0500

    Add pageview GAnalytics event

--- client/config/routes.js
+++ client/config/routes.js
@@ -2,6 +2,10 @@ Router.configure({
  layoutTemplate: 'application'
 })
 
+Router.onAfterAction(function(){
+  GAnalytics.pageview(this.path)
+})
+
 Router.map(function(){
   this.route('home', {
     path: '/'
